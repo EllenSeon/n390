42278f780f4e6b945546e56858fd6a31a9e43f4f
ELLEN SEON <seunghyeon.seon@navercorp.com>
2017-11-20 21:11:31 +0900

    project initialize

---
+++ N390/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  N390
+//
+//  Created by ELLEN SEON on 2017. 11. 20..
+//  Copyright © 2017년 ELLEN SEON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ N390/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  N390
+//
+//  Created by ELLEN SEON on 2017. 11. 20..
+//  Copyright © 2017년 ELLEN SEON. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
